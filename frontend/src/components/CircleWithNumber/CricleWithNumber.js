37ca6c1bd673b4032f729fe33d022343e03427f4
Oscar Spencer <oscarspencer@Oscars-MacBook-Air.local>
2023-11-11 14:43:04 -0700

    product card more info

---
+++ frontend/src/components/CircleWithNumber/CricleWithNumber.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const getColor = (number) => {
+  const red = Math.floor(255 - (number * 25.5));
+  const green = Math.floor(number * 25.5);
+  return `rgb(${red}, ${green}, 0)`;
+};
+
+const StyledCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  color: white;
+  font-size: 20px;
+  background-color: ${({ number }) => getColor(number)};
+`;
+
+export const CircleWithNumber = ({ number }) => {
+  return <StyledCircle number={number}>{number}</StyledCircle>;
+};
+
