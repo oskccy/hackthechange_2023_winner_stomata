6f886401f877efdf64e73cf03fc5bacb87f0cd66
Oscar Spencer <oscarspencer@Oscars-MacBook-Air.local>
2023-11-11 16:52:50 -0700

    camera component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
+import React from "react";
 import "./App.css";
-
-import Card, {
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
   RecommendedProductCard,
-} from "./components/ProductCards/RecommendedProductCard";
-
+} from "./components/ProductCard/ProductCard";
 import ScoreCard from "./components/Score/ScoreCard";
-import logo from "./logo.svg";
 import turtle from "./turtle.jpeg";
+import CameraComponent from "./components/CameraComponent/CameraComponent";
 
-function App() {
+function ProductPage() {
   return (
     <>
       <RecommendedProductCard
         title={"Product Title"}
-        metrics={[{"value": 4, "name": "carbon-1-metric", "points": ["yo", "yo", "yo"]}, {"value": 5, "name": "carbon-2 metric", "points": ["yo", "yo", "yo"]}, {"value": 8.9, "name": "other-metric", "points": ["yo", "yo", "yo"]}]}
+        metrics={[ // dummy data
+          { value: 4, name: "carbon-1-metric", points: ["yo", "yo", "yo"] },
+          { value: 5, name: "carbon-2 metric", points: ["yo", "yo", "yo"] },
+          { value: 8.9, name: "other-metric", points: ["yo", "yo", "yo"] },
+        ]}
         imgUrl={turtle}
       />
-
-      <ScoreCard score={5.9}/>
+      <ScoreCard score={5.9} />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="" element={<CameraComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/CameraComponent/CameraComponent.js
@@ -0,0 +1,56 @@
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+import styled from 'styled-components';
+
+const videoConstraints = {
+  width: 390,
+  height: 720,
+  facingMode: 'user',
+};
+
+const BackgroundDiv = styled.div`
+  background-color: green;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CaptureButton = styled.button`
+  position: fixed;
+  border:9px solid white;
+  border-radius:50%;
+  left: 41%;
+  top: 80%;
+  background:rgba(0,0,0,0);
+  color:white;
+  width:90px;
+  height:90px;
+  cursor: pointer;
+`;
+
+const CameraComponent = () => {
+  const webcamRef = useRef(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    // there will be pushing the image to the model
+    console.log(imageSrc);
+  };
+
+  return (
+    <BackgroundDiv>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        width={390}
+        height={844}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+      />
+      <CaptureButton onClick={capture}/>
+    </BackgroundDiv>
+  );
+};
+
+export default CameraComponent;
